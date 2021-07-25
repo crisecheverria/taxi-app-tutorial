665ed4836a3359aea2696e796bbc1c6dffe02348
Cristian Echeverria <cristian.echeverri4@gmail.com>
2021-07-25 15:16:47 +0200

    Added customMapStyles & props. Also new folders inside src.

--- src/App.js
+++ src/App.js
@@ -6,73 +6,11 @@
  * @flow strict-local
  */
 
-import React, {useEffect, useState} from 'react';
-import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
-import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
-import Geolocation from 'react-native-geolocation-service';
+import React from 'react';
+import UserScreen from './screens/UserScreen';
 
 const App = () => {
-  const [location, setLocation] = useState(null);
-
-  const handleLocationPermission = async () => {
-    const permissionCheck = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
-
-    if (permissionCheck === RESULTS.DENIED) {
-      const permissionRequest = await request(
-        PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
-      );
-      permissionRequest === RESULTS.GRANTED
-        ? console.warn('Location permission granted.')
-        : console.warn('Location perrmission denied.');
-    }
-  };
-
-  useEffect(() => {
-    handleLocationPermission();
-  }, []);
-
-  useEffect(() => {
-    Geolocation.getCurrentPosition(
-      position => {
-        const {latitude, longitude} = position.coords;
-        setLocation({latitude, longitude});
-      },
-      error => {
-        console.log(error.code, error.message);
-      },
-      {enableHighAccuracy: true, timeout: 15000, maximumAge: 10000},
-    );
-  }, []);
-
-  return (
-    <SafeAreaView style={styles.container}>
-      <StatusBar barStyle="dark-content" />
-      {location && (
-        <MapView
-          style={styles.map}
-          provider={PROVIDER_GOOGLE}
-          initialRegion={{
-            latitude: location.latitude,
-            longitude: location.longitude,
-            latitudeDelta: 0.0922,
-            longitudeDelta: 0.0421,
-          }}
-          showsUserLocation={true}
-        />
-      )}
-    </SafeAreaView>
-  );
+  return <UserScreen />;
 };
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-});
 
 export default App;
