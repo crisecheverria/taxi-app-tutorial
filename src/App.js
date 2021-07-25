87e0b36d5a76fc740fd8cf63cc4ed7842c864014
Cristian Echeverria <cristian.echeverri4@gmail.com>
2021-07-25 05:21:16 +0200

    Added show user location on Map

--- src/App.js
+++ src/App.js
@@ -6,11 +6,29 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React, {useEffect} from 'react';
 import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 import MapView, {PROVIDER_GOOGLE} from 'react-native-maps';
+import {check, request, PERMISSIONS, RESULTS} from 'react-native-permissions';
 
 const App = () => {
+  const handleLocationPermission = async () => {
+    const permissionCheck = await check(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+
+    if (permissionCheck === RESULTS.DENIED) {
+      const permissionRequest = await request(
+        PERMISSIONS.IOS.LOCATION_WHEN_IN_USE,
+      );
+      permissionRequest === RESULTS.GRANTED
+        ? console.warn('Location permission granted.')
+        : console.warn('Location perrmission denied.');
+    }
+  };
+
+  useEffect(() => {
+    handleLocationPermission();
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar barStyle="dark-content" />
@@ -23,6 +41,7 @@ const App = () => {
           latitudeDelta: 0.0922,
           longitudeDelta: 0.0421,
         }}
+        showsUserLocation={true}
       />
     </SafeAreaView>
   );
