23f45c9630a9a2ff408ac8ba93edd9d1e586a265
Cristian Echeverria <cristian.echeverri4@gmail.com>
2021-07-29 11:46:34 +0200

    Consume Context API

--- src/App.js
+++ src/App.js
@@ -8,9 +8,14 @@
 
 import React from 'react';
 import UserScreen from './screens/UserScreen';
+import {PlaceProvider} from './context/PlacesManager';
 
 const App = () => {
-  return <UserScreen />;
+  return (
+    <PlaceProvider>
+      <UserScreen />
+    </PlaceProvider>
+  );
 };
 
 export default App;
