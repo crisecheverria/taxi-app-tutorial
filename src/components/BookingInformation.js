e23bda55dc785b78926e7a9a449be45b9b6c9e88
Cristian Echeverria <cristian.echeverri4@gmail.com>
2021-08-14 19:59:21 +0200

    Passing currentPlace

--- src/components/BookingInformation.js
+++ src/components/BookingInformation.js
@@ -96,6 +96,7 @@ export default function BookingInformation() {
         toggleModal={togglePlaceModal}
         newAddress={newAddress}
         setNewAddress={setNewAddress}
+        currentPlace={currentPlace}
       />
     </>
   );
