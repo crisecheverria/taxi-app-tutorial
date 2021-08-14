14483e7f370456f7fef31bf1a47653c5ede1d342
Cristian Echeverria <cristian.echeverri4@gmail.com>
2021-08-14 19:58:56 +0200

    Added fetch api utility function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,16 @@
+import {GOOGLE_MAPS_API_KEY} from './constants';
+
 export const formatPlaceName = placeName =>
   placeName && placeName.split(',')[0];
+
+export const APIPlaceAutocomplete = (destination, currentPlace) => {
+  const URL = `https://maps.googleapis.com/maps/api/place/autocomplete/json?key=${GOOGLE_MAPS_API_KEY}&input=${destination}&location=${currentPlace.latitude},${currentPlace.longitude}&radius=2000`;
+
+  if (destination.length > 0) {
+    return fetch(URL)
+      .then(resp => resp.json())
+      .catch(error => error);
+  } else {
+    return 'No destination Address provided';
+  }
+};
